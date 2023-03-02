87a567a9a2dcb33edb9c9e68f868c560045ff227
Mongke22 <ddd.ddd99.99@mail.ru>
2023-03-02 13:34:20 +0300

    Сплеш скрин + пустой фрагмент с навигацией снизу

---
+++ app/src/main/java/com/example/astonfinalproject/CharacterFragment.kt
@@ -0,0 +1,25 @@
+package com.example.astonfinalproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CharacterFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_character, container, false)
+    }
+
+    companion object {
+
+        fun newInstance() = CharacterFragment()
+    }
+}
