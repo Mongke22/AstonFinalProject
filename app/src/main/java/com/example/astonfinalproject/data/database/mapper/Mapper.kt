a73dd02eba095ce698ad95b7bc44f101d96863b2
Mongke22 <ddd.ddd99.99@mail.ru>
2023-03-13 15:28:28 +0300

    Дополнительные методы в мапере

--- app/src/main/java/com/example/astonfinalproject/data/database/mapper/Mapper.kt
+++ app/src/main/java/com/example/astonfinalproject/data/database/mapper/Mapper.kt
@@ -6,39 +6,71 @@ import com.example.astonfinalproject.data.database.dbModels.LocationInfoDbModel
 import com.example.astonfinalproject.data.network.Model.Characters.CharactersResultDto
 import com.example.astonfinalproject.data.network.Model.Episodes.EpisodesResultDto
 import com.example.astonfinalproject.data.network.Model.Locations.LocationsResultDto
+import com.example.astonfinalproject.domain.Model.CharacterInfo
+import com.example.astonfinalproject.domain.Model.EpisodeInfo
+import com.example.astonfinalproject.domain.Model.LocationInfo
 
 class Mapper {
-    fun mapCharacterDtoToDbModel(characterDto: CharactersResultDto): CharacterInfoDbModel{
+    fun mapCharacterDtoToDbModel(characterDto: CharactersResultDto): CharacterInfoDbModel {
         return CharacterInfoDbModel(
-            characterDto.url?:"unknown",
-            characterDto.name?:"unknown",
-            characterDto.species?:"unknown",
-            characterDto.status?:"unknown",
-            characterDto.gender?:"unknown",
-            characterDto.image?:"unknown",
-            characterDto.origin?.name?:"unknown",
-            characterDto.origin?.url?:"unknown",
-            characterDto.location?.name?:"unknown",
-            characterDto.location?.url?:"unknown",
+            characterDto.url ?: "unknown",
+            characterDto.name ?: "unknown",
+            characterDto.species ?: "unknown",
+            characterDto.status ?: "unknown",
+            characterDto.gender ?: "unknown",
+            characterDto.image ?: "unknown",
+            characterDto.origin?.name ?: "unknown",
+            characterDto.origin?.url ?: "unknown",
+            characterDto.location?.name ?: "unknown",
+            characterDto.location?.url ?: "unknown",
             characterDto.episode
         )
     }
+
     fun mapEpisodeDtoToDbModel(episodeDto: EpisodesResultDto): EpisodeInfoDbModel {
         return EpisodeInfoDbModel(
-            episodeDto.url?:"unknown",
-            episodeDto.name?:"unknown",
-            episodeDto.air_date?:"unknown",
-            episodeDto.episode?:"unknown",
+            episodeDto.url ?: "unknown",
+            episodeDto.name ?: "unknown",
+            episodeDto.air_date ?: "unknown",
+            episodeDto.episode ?: "unknown",
             episodeDto.characters
         )
     }
+
     fun mapLocationDtoToDbModel(locationDto: LocationsResultDto): LocationInfoDbModel {
         return LocationInfoDbModel(
-            locationDto.url?:"unknown",
-            locationDto.name?:"unknown",
-            locationDto.type?:"unknown",
-            locationDto.dimension?:"unknown",
+            locationDto.url ?: "unknown",
+            locationDto.name ?: "unknown",
+            locationDto.type ?: "unknown",
+            locationDto.dimension ?: "unknown",
             locationDto.characters
+        )
+    }
+
+
+    fun mapCharacterDbModelToEntity(characterDb: CharacterInfoDbModel): CharacterInfo {
+        return CharacterInfo(
+            characterDb.name,
+            characterDb.imageSrc,
+            characterDb.status,
+            characterDb.gender,
+            characterDb.species
+        )
+    }
+
+    fun mapEpisodeDbModelToEntity(episodeDb: EpisodeInfoDbModel): EpisodeInfo {
+        return EpisodeInfo(
+            episodeDb.name,
+            episodeDb.episodeNumber,
+            episodeDb.airDate
+        )
+    }
+
+    fun mapLocationDbModelToEntity(locationDb: LocationInfoDbModel): LocationInfo {
+        return LocationInfo(
+            locationDb.name,
+            locationDb.type,
+            locationDb.dimension
         )
     }
 }
